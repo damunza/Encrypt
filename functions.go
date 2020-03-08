e45c6354f0502a5fa56ba7e6387f39406afd7c9a
damunza <danidamunza@gmail.com>
2020-03-08 20:32:48 +0300

    removed its child subpackage to solve a path error. now i have a whole new package system

--- functions.go
+++ functions.go
@@ -1,8 +1,10 @@
+//Package app contains functions and methods to create and deal with the app database
 package app
 
 // import db "Go_Projects/Encrypt/app/dbs"
 // import "fmt"
 
+// Something is just a method to pretest installation of the app package
 func Something(i string) string{
 	return "app connected" + i
 }
