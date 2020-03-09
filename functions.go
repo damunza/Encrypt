e1bf3ff270af02b6d4a1c8ce36e5afaa7743ca0e
damunza <danidamunza@gmail.com>
2020-03-09 10:12:52 +0300

    moved the declaration into a function

--- functions.go
+++ functions.go
@@ -1,10 +1,12 @@
 //Package app contains functions and methods to create and deal with the app database
 package app
 
-// import db "Go_Projects/Encrypt/app/dbs"
-// import "fmt"
+//_"Go_Projects/Encrypt/app/dbs" import to prevent exposing secretkey
+import "Go_Projects/Encrypt/app/dbs"
+import "fmt"
 
 // Something is just a method to pretest installation of the app package
 func Something(i string) string{
+	fmt.Println(dbs.dbLink())
 	return "app connected" + i
 }
