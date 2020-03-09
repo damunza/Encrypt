d0c1d90a35a66723c5f676dc34df57d745a5f308
damunza <danidamunza@gmail.com>
2020-03-09 15:35:58 +0300

    i think i solved all the errors testing to see

--- functions.go
+++ functions.go
@@ -1,16 +1,16 @@
 //Package app contains functions and methods to create and deal with the app database
 package app
 
-import _ "Go_Projects/Encrypt/dbs" // prevent exposure of secret keys 
-import "fmt"
+import (
+	"Go_Projects/Encrypt/dbs"
+	_ "Go_Projects/Encrypt/dbs" // prevent exposure of secret keys 
+	// "fmt"
+)
 
 // Something is just a method to pretest installation of the app package
 func Something(i string) string{
+	dbs.Run()
 	return "app connected" + i
 }
 
-// testing if db is connected
-func Db(){
-	fmt.Println(dbs.DbLink())
-}
 
