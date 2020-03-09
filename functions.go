41a3b637514b93472894ea9599cd32e5335c041e
damunza <danidamunza@gmail.com>
2020-03-09 10:39:22 +0300

    reinstalling doesnt work so far

--- functions.go
+++ functions.go
@@ -6,7 +6,9 @@ import "fmt"
 
 // Something is just a method to pretest installation of the app package
 func Something(i string) string{
-	db := dbs.dbLink()
-	fmt.Println(db)
 	return "app connected" + i
+}
+
+func dB(){
+	fmt.Println(dbs.dbLink())
 }
